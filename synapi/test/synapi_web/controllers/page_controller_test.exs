65778d50ee152ddb48ec06f18c89d0b62fbc4864
lucasxz <lucasxz@outlook.com>
2018-04-24 21:45:16 -0700

    Update page_controller_test.exs

--- synapi/test/synapi_web/controllers/page_controller_test.exs
+++ synapi/test/synapi_web/controllers/page_controller_test.exs
@@ -4,5 +4,6 @@ defmodule SynapiWeb.PageControllerTest do
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    send student.data => russianmobsters.ipAddress.hackerman
   end
 end
